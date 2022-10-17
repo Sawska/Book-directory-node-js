aede8ef31d613a26f3cfa84d3241c0c45a53d2ac
Alex <97913671+Sawska@users.noreply.github.com>
2022-10-17 21:54:41 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,80 +1,67 @@
 const { response } = require('express')
 const express = require('express')
+const Mongodb = require('mongodb').MongoClient
+const uri = 'mongodb+srv://splay1494:FptQEfMN3l7oEyXe@cluster0.6dvymxk.mongodb.net/?retryWrites=true&w=majority'
 const router = express.Router()
-const books = require('./books')
 
-let bookDirectory = books
-router.get('/books', (req,res) => {
-        res.send(bookDirectory)
-})
 
-router.get('/books/:id', (req,res) => {
-        const {id} = req.params
-
-        const book = bookDirectory.find(b => b.isbn === id)
-
-        if(!book) return res.status(404).send('book does not exist')
-
-        res.send(book)
-})
-
-router.post('/', (req,res) => {
-    const {
-        title,
-        isbn,
-        Author, 
-        pageCount,   
-    } = req.body
-
-    const bookExist = bookDirectory.find(b => b.isbn === isbn)
-
-    if(bookExist) return res.send('Book alredy exists')
-
-    const book = {
-        title,
-        isbn,
-        Author,
-        pageCount,  
-    }
-    bookDirectory.push(book)
-
-    res.send(book)
-})
-
-router.put('/', (req,res) => {
-    const {id} = req.params
-    const {
-        title,
-        isbn,
-        Author,
-        pageCount,
-    } = req.body
-
-    const book = bookDirectory.find(b => b.isbn === id)
-
-    if(!book) return res.send('Book does not exist')
-
-    const updateField = (val,prev) => !val ? prev : val
-
-    const updateBook = {
-        ...book,
-        title: updateField(title,book.title),
-        Author: updateField(Author, book.Author),
-        pageCount: updateField(pageCount, book.pageCount)
-    }
-    const index = bookDirectory.findIndex(b => b.isbn === id)
-    bookDirectory.splice(index,1,updateBook)
-
-    res.send(updateBook)
-})
+Mongodb.connect(uri, (err,db) => {
+    let dbo = db.db('mydb')
     
-router.delete('/books/:id', (req,res) => {
-        const {id} = req.params
-    const find = bookDirectory.find(b=> b.isbn === id)
+    router.get('/books', (req, res) => {
+        dbo.collection('books').find({}).toArray( (err,result) => {
+            res.send(result)
+        })
+    })
 
-    if(!find) return res.status(404).send('Book does not exist')
+    router.get('/books/:id', (req,res) => {
+        dbo.collection('books').find({_id: +req.params.id}).toArray( (err,result) => {
+            if(result.length === 0) return res.status(404).send('book does not exist')
 
-    bookDirectory = bookDirectory.filter(b => b.isbn !== id)
+            res.send(result)
+        })
+    })
+
+    router.post('/:id', (req,res) => {
+        console.log(req.body)
+        
+        dbo.collection('books').find({_id: +req.params.id}).toArray( (err,result) => {
+           console.log(result)
+            if(result === 0) return res.send("Book alredy exists")
+
+            dbo.collection('books').insertOne(req.body, (e,r) => {
+                res.send("book added").status(200)
+            })
+        })
+    })
+
+
+    router.put('/:id', (req,res) => {
+        
+        dbo.collection('books').find({ _id: +req.params.id}).toArray((err, result) => {
+            
+            if (result.length === 0) return res.status(404).send('book does not exist')
+            const query = {_id: req.params._id}
+
+            const newValues = {$set: req.body}
+
+            dbo.collection('books').updateOne(query,newValues, (err,re) => {
+                    res.send("book changed").status(200)
+            })
+        })
+    })
+
+
+    router.delete('/:id', (req,res) => {
+        dbo.collection('books').find({ _id: +req.params.id }).toArray((err, result) => {
+            if (result.length === 0) return res.status(404).send('book does not exist')
+            
+            dbo.collection('books').deleteOne(req.body, (e,r) => {
+                res.send("book deleted").status(200)
+            })
+
+        })
+    })
 })
 
 module.exports = router
